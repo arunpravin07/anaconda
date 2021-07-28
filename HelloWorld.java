25178bf87591fd6d3b3ffd63fb0b716661133919
arunpravin07 <43900333+arunpravin07@users.noreply.github.com>
2021-07-28 17:41:31 +0530

    del space

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("vaa yaa  ...  enn veera"); 
-        System.out.println("vaa yaa  ...   enn veera"); 
-     
-       
-         
+        System.out.println("vaa yaa  ...   enn veera");
        
     }
 }
