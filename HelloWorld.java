9b64320adcd0c4324d68bbb6fe9182421903e12b
arunpravin07 <43900333+arunpravin07@users.noreply.github.com>
2021-07-28 17:50:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("vaa yaa  ...  enn veera"); 
         System.out.println("vaa yaa  ...   enn veera");
-     System.out.println("vaa yaa  ...veera");  
+     System.out.println("vaa   ...veera");  
         
     }
 }
